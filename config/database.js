f667040ba7cc107e09f8269ec28b05b411d78218
Miri <miriamkifle.dev@gmail.com>
2021-11-06 20:14:30 -0400

    social-app

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbUser:<password>@cluster0.om3uc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbUser': 'social-app'
+};
